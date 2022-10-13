fd8c125e16b63b35ffed753df8f9f3cd27192a15
DanFariasM <danfariasm@gmail.com>
2022-10-12 20:50:31 -0400

    Catch-up update on Sideways Shooter

---
+++ game_stats.py
@@ -0,0 +1,15 @@
+class GameStats:
+    """Track statistics for Alien Invasion."""
+
+    def __init__(self, ss_game):
+        """Initialize statistics."""
+        self.settings = ss_game.settings
+        self.reset_stats()
+
+        # Start Alien Invasion in an active state.
+        self.game_active = True
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
+
